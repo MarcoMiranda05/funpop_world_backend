0a06f60f206dc96f564cd5150577ab4a97fdde1a
MarcoMiranda05 <marmirand@hotmail.com>
2019-07-31 18:15:12 +0100

    heroku check

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,7 +2,7 @@ class AuthController < ApplicationController
   def create
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
-      console.log(user)
+      puts user
       payload = {user_id: user.id}
       token = issue_token(payload)
       render json: { jwt: token }
