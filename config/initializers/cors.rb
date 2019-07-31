dbff1653a4cbc142db5f387b709afdc1d94d94b7
MarcoMiranda05 <marmirand@hotmail.com>
2019-07-31 16:01:44 +0100

    cors to netlify

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://funpop-world.netlify.com/'
+    origins '*'
 
     resource '*',
       headers: :any,
